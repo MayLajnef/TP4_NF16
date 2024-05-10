f928776c6913e6dfa47b5420595a0b33e5adcb53
MayLajnef <125302971+MayLajnef@users.noreply.github.com>
2024-05-10 23:27:40 +0200

    Update tp4.h

--- tp4.h
+++ tp4.h
@@ -1,6 +1,33 @@
 #ifndef TP4_H
 #define TP4_H
 
+typedef struct Sommet
+{
+    int borneInf;
+    int borneSup;
+    struct Sommet* filsGauche;
+    struct Sommet* filsDroit;
+}T_Sommet;
+
+typedef T_Sommet* T_Arbre;
+
+T_Sommet *creerSommet(int element);
+
+T_Arbre insererElement(T_Arbre abr, int element);
+
+T_Sommet *rechercherElement(T_Arbre abr, int element);
+
+void afficherSommets(T_Arbre abr);
+
+void afficherElements(T_Arbre abr);
+
+T_Arbre supprimerElement(T_Arbre abr, int element);
+
+void tailleMemoire(T_Arbre abr);
+
+void libererAbr(T_Arbre abr);
+
+
 
 
 #endif //TP4_H
