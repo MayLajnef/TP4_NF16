fd45de418379b61f9830705f74f45758026d8298
MayLajnef <125302971+MayLajnef@users.noreply.github.com>
2024-05-11 09:38:46 +0200

    mise à jour

--- main.c
+++ main.c
@@ -2,7 +2,47 @@
 #include <stdlib.h>
 #include "tp4.h"
 
-int main()
-{
+int main() {
+
+    T_Arbre abr = NULL;
+
+    while (1) {
+        printf("1. . Inserer N elements \n");
+        printf("2. Rechercher un element \n");
+        printf("3. Afficher tous les sommets \n");
+        printf("4. Afficher tous les elements \n");
+        printf("5. Supprimer un element \n");
+        printf("6. Afficher la taille en memoire \n");
+        printf("7. Quitter \n");
+        int input;
+        scanf("%d", &input);
+        switch (input) {
+            case 1:
+                printf("Insertion de N elements...\n");
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            case 4:
+                
+                break;
+            case 5:
+                
+                break;
+            case 6:
+                
+                break;
+            case 7:
+                libererAbr(abr) ; 
+                exit(1);
+            default:
+                printf("Option non valide. Veuillez choisir une option entre 1 et 7.\n");
+                break;
+        }
+    }
+
     return 0;
 }
